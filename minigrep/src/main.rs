3aa1fc12159924071ed09d94829ebe18734c082d
mohseenrm <mohseenmukaddam6@gmail.com>
2024-10-14 14:46:55 -0700

    refactor main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,34 @@
 use std::env;
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-    println!("Hello, world!");
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents);
 }
