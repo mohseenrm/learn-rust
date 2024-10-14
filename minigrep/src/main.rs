d11a1571173a1dd800e2a893447f1f83892b1c07
mohseenrm <mohseenmukaddam6@gmail.com>
2024-10-14 14:24:59 -0700

    add minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    println!("Hello, world!");
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
+}
