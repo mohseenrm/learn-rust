dac41319581ea2fd6bc9b80867860c824c027193
mohseenrm <mohseenmukaddam6@gmail.com>
2024-10-14 17:06:35 -0700

    add case insensitive search

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,23 +1,6 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -28,7 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
