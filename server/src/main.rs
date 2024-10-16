5e06d91f747acc167ceee134f4e767ff6486ed01
mohseenrm <mohseenmukaddam6@gmail.com>
2024-10-16 13:51:32 -0700

    update graceful shutdown threshold

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming().take(5) {
         let stream = stream.unwrap();
 
         println!("Connection established!");
